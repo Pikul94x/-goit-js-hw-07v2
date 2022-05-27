2651aa07b7b933c4ec2562bff5f594713a65a774
Pikul94 <92268463+Pikul94@users.noreply.github.com>
2022-05-27 22:32:14 +0200

    initial commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// console.log(galleryItems)
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+	.map(
+		item => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+	)
+	.join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", e => {
+	e.preventDefault();
+	if (e.target.nodeName !== "IMG") return;
+
+	const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`);
+
+	instance.show();
+
+	document.addEventListener("keydown", e => {
+		if (e.key === "Escape") {
+			instance.close();
+		}
+	});
+});
